5fa803b45daf9a05d074996cf70f465ae22585c8
nabsba <ns-dev@outlook.com>
2022-06-22 13:48:47 +0100

    feat(_documents.tsx): add links for police

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="anonymous"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@500&family=Fredoka+One&family=Inter&display=swap"
+						rel="stylesheet"
+					/>
+					{/* Police bilo  */}
+					<link rel="stylesheet" href="https://use.typekit.net/rua6gmq.css" />
+
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;

a6bac75dd4b7ca9515745b96fa184f2251ea8b2c
nabsba <ns-dev@outlook.com>
2022-06-25 04:00:05 +0100

    style(LeftSideV211): increase padding

--- components/tree/organisms/LeftSideV211.tsx
+++ components/tree/organisms/LeftSideV211.tsx
@@ -19,7 +19,7 @@ const LeftV211: React.FC<TLeftV211> = ({ annonce }) => {
     const { viewPort } = React.useContext(ViewPortChildContext);
     const theme: any = useTheme();
 
-    return (<div className="flex_column_center_between" style={{ flex: 1, paddingTop: '1rem' }}>
+    return (<div className="flex_column_center_between" style={{ flex: 1, paddingTop: '1.5rem' }}>
         <CSSAnnonceOriginal
             viewPortWidth={viewPort.width}
             background={theme.COLORS.PRIMARY}>

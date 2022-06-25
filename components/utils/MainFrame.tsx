795fb9aaa7c8f8b5a3145b208152069aa4f89aab
nabsba <ns-dev@outlook.com>
2022-06-25 12:54:26 +0100

    style(MainFrame.tsx): add responsive screen'

--- components/utils/MainFrame.tsx
+++ components/utils/MainFrame.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import { css } from '@emotion/react';
 import ViewPortParentContext, { ViewPortChildContext } from './contexts/ViewPort';
+import { SIZE_ELEMENTS_ACTUAL_VIEW_PORT } from '../../services';
 
 type TMainFrame = {
   children: any;
 }
 // height: ${SIZE_ELEMENTS_ACTUAL_VIEW_PORT.MAIN_SIDE_HEIGHT(viewPort.width)}px;
 const MainFrame: React.FC<TMainFrame> = ({ children }) => {
+
   const { handleViewPortInformations, viewPort } = ViewPortParentContext();
   const base: any = css`
 height: 100vh;
+height: ${viewPort.width > 790 ? '100vh' : SIZE_ELEMENTS_ACTUAL_VIEW_PORT.MAIN_SIDE_HEIGHT(viewPort.width)}px;
 width: 100%;
 margin: auto;
-height: 100vh;
 padding:.5rem;
 
 `

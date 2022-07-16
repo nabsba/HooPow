0d33453e71c6af6127dd11fcd2d2e5e57bd09d99
nabsba <ns-dev@outlook.com>
2022-07-16 15:10:55 +0100

    test(test-utils.tsx): pass theme context

--- services/jest/config/test-utils.tsx
+++ services/jest/config/test-utils.tsx
@@ -5,13 +5,15 @@ import {
 	createMemorySource,
 	LocationProvider,
 } from '@reach/router';
+import { ThemeProvider } from '@emotion/react';
+import { THEME } from '../../style/constant';
 
 const renderWithRouter = (
 	ui: React.ReactNode,
 	{ route = '/', history = createHistory(createMemorySource(route)) } = {},
 ) => {
 	return {
-		...render(<LocationProvider history={history}>{ui}</LocationProvider>),
+		...render(<ThemeProvider theme={THEME}><LocationProvider history={history}>{ui}</LocationProvider> </ThemeProvider>),
 		history,
 	};
 };
